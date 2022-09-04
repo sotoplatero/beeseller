65cc756d412e1b44dd8b02c60c58d1830e736f6c
Damian Soto <soto.platero@gmail.com>
2022-09-04 20:51:01 -0300

    decorection tile home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 			<div class="">
 				<img srcset="/cropped-iso.webp" type="image/webp" alt="Logo de Beseller" class="w-24 mx-auto"/>
 			</div>
-			<div>Crea tu tienda <span class="font-bold text-orange-500">Gratis</span></div>
+			<div class="relative">Crea tu tienda <span class="font-bold text-orange-500 z-20 relative">Gratis<span class="w-full z-10 transition-all absolute ease-out duration-300 bg-bee/60 h-3 sm:h-6 bottom-0 left-0 skew-x-12 -rotate-3"></span></span></div>
 	
 		</h1>
 	
@@ -42,7 +42,10 @@
 				</div>
 				<div>
 					<button type class="btn primary text-lg">
-						Comenzar a Vender
+						<span class="hidden sm:inline mr-2">Comenzar a Vender</span>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+						  </svg>
 					</button>
 				</div>
 			</form>
